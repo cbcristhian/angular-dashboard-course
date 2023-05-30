3a7508fe0c0dc921cc1204ba76ed56248abeefcb
cbcristhian <cristhian.castro@unosquare.com>
2023-05-30 17:39:47 -0400

    styles configured and basic routing

---
+++ src/app/pages/graphics1/graphics1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graphics1',
+  templateUrl: './graphics1.component.html',
+  styleUrls: ['./graphics1.component.css']
+})
+export class Graphics1Component {
+
+}
